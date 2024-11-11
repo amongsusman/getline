aaab0aa794a67313247826e9edea1b0279366e47
amongsusman <149024306+amongsusman@users.noreply.github.com>
2024-11-11 09:09:32 -0500

    Add files via upload

---
+++ pra.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+    int times;
+    string cur;
+    string category;
+    vector<string> list;
+    cout << "Create a top X list of one of these categories: Movies, TV Shows, Songs, YouTubers, Video Games" << endl;
+    cout << "Which category would you like to select?" << endl;
+    getline(cin, category);
+    cout << "How long do you want your list to be?" << endl;
+    cin >> times;
+    cin.ignore();
+    for (int i = 0; i < times; i++) {
+        getline(cin, cur);
+        list.push_back(cur);
+    }
+    for (int i = 0; i < list.size(); i++) {
+        cout << list[i] << endl;
+    }
+    return 0;
+}
